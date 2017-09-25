1cf067b77eab38ca47d3cfa5ae3f4e38cd982715
Mike J Hewitt <mikeh@electroteach.com>
2017-09-26 07:42:58 +1000

    build tools

--- Music/Instrument/Chord.hs
+++ Music/Instrument/Chord.hs
@@ -1,61 +1,37 @@
 module Music.Instrument.Chord
-(
- renderGuitarConcept
- ,
- standardTuning
- ,
- dropD
- ,
- ControlAnnotation (..)
- ,
- renderPianoConcept
- ,
- module Music.Diatonic.Note
- ,
- module Music.Diatonic.Degree
- ,
- module Music.Diatonic
- ,
- module Music.Diatonic.Chord
- ,
- module Music.Diatonic.Scale
- ,
- module Music.Diatonic.Harmony
- ,
- ukelele
- ,
- lightChord
- ,
- powerChord
- ,
- fifthChord
- ,
- findPositionPatterns
- ,
- getPositionPatternRange
- ,
- sus2
- ,
- sus4
- , 
- chordToScale
- ,
- slash
- ,
- hConcat
- ,
- standardWidths
-)
-where
+  ( renderGuitarConcept
+  , standardTuning
+  , dropD
+  , ControlAnnotation(..)
+  , renderPianoConcept
+  , module Music.Diatonic.Note
+  , module Music.Diatonic.Degree
+  , module Music.Diatonic
+  , module Music.Diatonic.Chord
+  , module Music.Diatonic.Scale
+  , module Music.Diatonic.Harmony
+  , ukelele
+  , lightChord
+  , powerChord
+  , fifthChord
+  , findPositionPatterns
+  , getPositionPatternRange
+  , sus2
+  , sus4
+  , chordToScale
+  , slash
+  , hConcat
+  , standardWidths
+  ) where
 
+import Music.Instrument.Common
 import Music.Instrument.Guitar
 import Music.Instrument.GuitarRender
 import Music.Instrument.Piano
-import Music.Instrument.Common
 
 import Music.Diatonic
-import Music.Diatonic.Note
-import Music.Diatonic.Degree
 import Music.Diatonic.Chord
-import Music.Diatonic.Scale
+import Music.Diatonic.Degree
 import Music.Diatonic.Harmony
+import Music.Diatonic.Note
+import Music.Diatonic.Scale

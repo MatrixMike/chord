d98c4632a6b6c2beac63e48b10508721ffa09950
Mike J Hewitt <mikeh@electroteach.com>
2017-09-26 13:14:34 +1000

    my explanation..

--- Music/Instrument/Common.hs
+++ Music/Instrument/Common.hs
@@ -1,30 +1,32 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE IncoherentInstances #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-module Music.Instrument.Common where 
+module Music.Instrument.Common where
 
-import Music.Diatonic hiding (transpose)
-import Music.Diatonic.Note hiding (transpose)
-import Music.Diatonic.Degree
-import Music.Diatonic.Chord
-import Music.Diatonic.Scale
-import Music.Diatonic.Harmony
 import Control.Monad
+import Data.Char
 import Data.List
 import Data.Maybe
-import Data.Char
 import qualified Data.Set
+import Music.Diatonic hiding (transpose)
+import Music.Diatonic.Chord
+import Music.Diatonic.Degree
+import Music.Diatonic.Harmony
+import Music.Diatonic.Note hiding (transpose)
+import Music.Diatonic.Scale
 
 class NewNotes a where
   newNotes :: a -> [Note]
   lastIntervals :: a -> NewSteps
   lastIntervals a = NewSteps se . notesToSteps . newNotes $ a
-    where se = requiresSequence' a
+    where
+      se = requiresSequence' a
   requiresSequence' :: a -> Bool
   getChords :: a -> Maybe [Chord]
   isSlash :: a -> Bool
-  isSlash = const False 
+  isSlash = const False
 
 instance NewNotes Chord where
   newNotes = notes
@@ -60,7 +62,7 @@ instance NewNotes NewSteps where
 instance NewNotes Harmony where
   newNotes = undefined
   requiresSequence' _ = undefined
-  getChords = Just . chords 
+  getChords = Just . chords
 
 instance NewNotes [Chord] where
   newNotes = undefined
@@ -68,13 +70,15 @@ instance NewNotes [Chord] where
   getChords = Just
 
 instance Nts NewChord where
-  notes (NewChord inversion b c) = applyMaybe (\x y-> y:x) (x:canonize(tf y):xs) inversion
-    where 
-    (x:y:xs) = notes c 
-    tf = case b of
-        (Just (Sus 2)) -> (\x -> flat (flat x))
-        (Just (Sus 4)) -> (\x -> (sharp x))
-        _ -> id
+  notes (NewChord inversion b c) =
+    applyMaybe (\x y -> y : x) (x : canonize (tf y) : xs) inversion
+    where
+      (x:y:xs) = notes c
+      tf =
+        case b of
+          (Just (Sus 2)) -> (\x -> flat (flat x))
+          (Just (Sus 4)) -> (\x -> (sharp x))
+          _ -> id
 
 instance NewNotes NewChord where
   newNotes = notes
@@ -87,6 +91,7 @@ sus n c = NewChord Nothing (Just $ Sus n) c
 slash note chord = NewChord (Just note) Nothing chord
 
 getNoteIndexFromChord note chord = elemIndex note (notes chord)
+
 getNoteFromChordIndex chordIndex chord = (notes chord) !! chordIndex
 
 {-
@@ -95,57 +100,85 @@ sus2 chord = (x:canonize(flat(flat y)):xs)
 sus4 chord = (x:canonize(sharp y):xs)
   where (x:y:xs) = newNotes chord
 -}
+data NewScale =
+  NewScale Chord
 
-data NewScale = NewScale Chord
-data NewSteps = NewSteps Bool [Int]
+data NewSteps =
+  NewSteps Bool
+           [Int]
 
-data NewChord = NewChord (Maybe Note) (Maybe ChordModifier) Chord
+data NewChord =
+  NewChord (Maybe Note)
+           (Maybe ChordModifier)
+           Chord
 
-data ChordModifier = Sus Int
+data ChordModifier =
+  Sus Int
 
-applyMaybe f a m = case m of
-  Just b -> f a b
-  _ -> a
+applyMaybe f a m =
+  case m of
+    Just b -> f a b
+    _ -> a
 
 instance Show ChordModifier where
- show (Sus x) = "sus"++show x
+  show (Sus x) = "sus" ++ show x
 
 instance Show NewChord where
- show (NewChord inversion modifier c) = applyMaybe (\x y -> x ++ "/" ++show y) (applyMaybe (\x y -> x++show y) (show c) modifier) inversion
+  show (NewChord inversion modifier c) =
+    applyMaybe
+      (\x y -> x ++ "/" ++ show y)
+      (applyMaybe (\x y -> x ++ show y) (show c) modifier)
+      inversion
 
 stepMap f (NewSteps b d) = NewSteps b (f d)
+
 deStep (NewSteps b xs) = xs
+
 getSteps (NewSteps b xs) = xs
 
-convertToSteps = lastIntervals 
-shiftOctave n = shiftStep (n*12)
-shiftStep n = stepMap (map (+n))
+convertToSteps = lastIntervals
+
+shiftOctave n = shiftStep (n * 12)
+
+shiftStep n = stepMap (map (+ n))
 
 chordToScale = NewScale
 
 stepToNote interval = chromaticScale !! (interval `rem` chromaticScaleLength)
-notesToSteps notes = concatMap (\(o,y) -> map (\i -> i + (o*chromaticScaleLength)  ) y ) $ zip [0..] (properGroup (\x y -> x > y) (map noteToChromaticIndex notes))
 
-data ControlAnnotation = AnnotateNote | AnnotatePositionVertical | AnnotatePositionHorizontal | AnnotateMarking
+notesToSteps notes =
+  concatMap (\(o, y) -> map (\i -> i + (o * chromaticScaleLength)) y) $
+  zip [0 ..] (properGroup (\x y -> x > y) (map noteToChromaticIndex notes))
+
+data ControlAnnotation
+  = AnnotateNote
+  | AnnotatePositionVertical
+  | AnnotatePositionHorizontal
+  | AnnotateMarking
 
 abbreviateNote x = "CdDeEFgGaAbB" !! fromJust (elemIndex x chromaticScale)
-    
-chromaticScale = [C,sharp C,D,sharp D,E,F,sharp F,G,sharp G,A,sharp A,B]
+
+chromaticScale =
+  [C, sharp C, D, sharp D, E, F, sharp F, G, sharp G, A, sharp A, B]
 
 chromaticScaleLength = length chromaticScale
 
-tuningAndPosToNote stringTuning pos = canonize $ applyNTimes sharp pos stringTuning
+tuningAndPosToNote stringTuning pos =
+  canonize $ applyNTimes sharp pos stringTuning
 
 applyNTimes f n x = iterate f x !! n
 
-noteToChromaticIndex note = fromJust (findIndex (flip equiv note) chromaticScale)
+noteToChromaticIndex note =
+  fromJust (findIndex (flip equiv note) chromaticScale)
 
-extractDegrees' concept = map (+ (noteToChromaticIndex root')) $ map (semitones . distance root' ) notes'
- where 
- root' = head notes'
- notes' = newNotes concept
+extractDegrees' concept =
+  map (+ (noteToChromaticIndex root')) $ map (semitones . distance root') notes'
+  where
+    root' = head notes'
+    notes' = newNotes concept
 
-degreeToChromaticIndex degree = fromJust (findIndex (flip equiv degree) degreeScale)
+degreeToChromaticIndex degree =
+  fromJust (findIndex (flip equiv degree) degreeScale)
 
 degreeScale = iterate (noteMap sharp) First
 
@@ -153,71 +186,109 @@ levelChord = map (flip mod chromaticScaleLength)
 
 inversions = map sequenceDegrees . rotations
 
-rotations = reverse . (\list -> map (\n -> (take (length list) . drop (length list -n)) (cycle list)) [1..length list])
+rotations =
+  reverse .
+  (\list ->
+     map
+       (\n -> (take (length list) . drop (length list - n)) (cycle list))
+       [1 .. length list])
 
-sequenceDegrees ds = scanl1 (\x y-> x + mod (y-x) chromaticScaleLength) ds
+sequenceDegrees ds = scanl1 (\x y -> x + mod (y - x) chromaticScaleLength) ds
 
-findChord inputNotes = do 
- chordType <- chordTypes
- root <- chromaticScale
- let notes = chordToNotes (chordType root)
- guard (Data.Set.isSubsetOf (Data.Set.fromList (uns inputNotes )) (Data.Set.fromList notes))
- return (chordType root) 
- where uns = map canonize
+findChord inputNotes = do
+  chordType <- chordTypes
+  root <- chromaticScale
+  let notes = chordToNotes (chordType root)
+  guard
+    (Data.Set.isSubsetOf
+       (Data.Set.fromList (uns inputNotes))
+       (Data.Set.fromList notes))
+  return (chordType root)
+  where
+    uns = map canonize
 
-chordTypes = [majorChord, minorChord, diminishedChord, augmentedChord,
-              major7thChord, dominant7thChord, minor7thChord, minorMajor7thChord, minor7thFlat5thChord, diminished7thChord, augmentedMajor7thChord]
+chordTypes =
+  [ majorChord
+  , minorChord
+  , diminishedChord
+  , augmentedChord
+  , major7thChord
+  , dominant7thChord
+  , minor7thChord
+  , minorMajor7thChord
+  , minor7thFlat5thChord
+  , diminished7thChord
+  , augmentedMajor7thChord
+  ]
 
 chordToNotes chord = map snd $ degrees chord
 
-horizontalConcat str1 str2 = unlines $ horizontalConcat' (lines str1) (lines str2)
+horizontalConcat str1 str2 =
+  unlines $ horizontalConcat' (lines str1) (lines str2)
+
 horizontalConcat' str1 str2 = zipWith (++) str1 str2
 
-deepenListOfListsAndAddEmpties =  addEmpties . deepenListOfLists
+deepenListOfListsAndAddEmpties = addEmpties . deepenListOfLists
+
 deepenListOfLists = map deepenList
 
-deepenList = map (:[])
-addEmpties = map ([]:)
-addEmptiesToEmpties xs = map (\x -> applyIf (null x) ([]:) x) xs
+deepenList = map (: [])
+
+addEmpties = map ([] :)
+
+addEmptiesToEmpties xs = map (\x -> applyIf (null x) ([] :) x) xs
 
 findIndicess p xs ys = map (\x -> findIndices (p x) xs) ys
 
-demoEquiv string number = length string == (length (show number))
+demoEquiv string number = length string == length (show number)
 
 uniqueBy eq l = uniqueBy' l []
   where
     uniqueBy' [] _ = []
     uniqueBy' (y:ys) xs
       | elem_by eq y xs = uniqueBy' ys xs
-      | otherwise = y : uniqueBy' ys (y:xs)
+      | otherwise = y : uniqueBy' ys (y : xs)
 
 elem_by :: (a -> a -> Bool) -> a -> [a] -> Bool
 elem_by _ _ [] = False
 elem_by eq y (x:xs) = x `eq` y || elem_by eq y xs
 
 applyIf :: Bool -> (a -> a) -> a -> a
-applyIf p f v = if p then f v else v
+applyIf p f v =
+  if p
+    then f v
+    else v
 
-insertAt = (\n x xs -> case splitAt n xs of { (a, b) -> a ++ [x] ++ b })
+insertAt =
+  (\n x xs ->
+     case splitAt n xs of
+       (a, b) -> a ++ [x] ++ b)
 
 properGroup f (x:xs) = properGroup' f [x] xs
-properGroup f [] = [] 
-properGroup' f buf@(_:_) (x:xs) = if f x (last buf) then properGroup' f (buf++[x]) xs
-                                                    else buf : properGroup' f [x] (xs)
+properGroup f [] = []
+
+properGroup' f buf@(_:_) (x:xs) =
+  if f x (last buf)
+    then properGroup' f (buf ++ [x]) xs
+    else buf : properGroup' f [x] (xs)
 properGroup' f buf [] = [buf]
 
+overlay xs ys = map head $ transpose [xs, ys]
 
-overlay xs ys = map head $ transpose [xs,ys]
+hAppend w w2 =
+  unlines $
+  map concat $
+  transpose $
+  map
+    (\lines ->
+       map
+         (\line -> overlay line (replicate (maximum (map length lines) + 1) ' '))
+         lines) $
+  (\documents ->
+     map
+       (\document ->
+          overlay document (replicate (maximum (map length documents)) " "))
+       documents)
+    [lines w, lines w2]
 
-hAppend w w2 =   unlines 
-               $ map concat
-               $ transpose 
-               $  map (\lines ->  
-                    map (\line ->  
-                        overlay line (replicate (maximum (map length lines)+1) ' ')
-                    ) lines 
-                  )
-               $ (\documents -> map (\document -> overlay document (replicate (maximum (map length documents)) " ")) documents) [lines w,lines w2]
-               
-                
 hConcat = foldl1 hAppend
---
+++ ReadmeMJH.txt
@@ -0,0 +1,10 @@
+Tue 26 Sep 2017 13:06:49 AEST 
+I found this useful to learn from. I have hidden musical interests and the project builds and executes so that is a good start.
+I followed suggestions from hlint and used hindent.
+I chose this to work on because it uses libraries and responds nicely to 'cabal repl' ==> use :browse
+
+I used to build with stack build but this project responds better to cabal build but gives some warnings and suggestions which I intend to follow up.
+
+This sequence when executed suggests some improvements.
+cabal clean && cabal build
+
